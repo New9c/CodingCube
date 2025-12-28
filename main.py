cd71bca8771315e99ecb88acae91b60100b48e6d
New9c <aimccccccccc@gmail.com>
2025-12-28 22:57:54 +0800

    Add forward face

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import math
 import mymath
 import mic
+from enum import Enum
 
 # pygame setup
 pygame.init()
@@ -20,6 +21,30 @@ offset = 35
 
 NORMAL_FONT = pygame.font.SysFont("JetBrainsMonoNerdFont-Regular.ttf", 20)
 TALKING_FONT = pygame.font.SysFont("JetBrainsMonoNerdFont-Regular.ttf", 25)
+class EyesDirection(Enum):
+    FORWARD = 0
+    LEFT_TOP = 1
+eyes = EyesDirection.LEFT_TOP
+
+def draw_eyes():
+    if mic.talking_frames<=0:
+        pygame.draw.circle(screen, (255, 255, 255), (205-offset, 200+4*math.sin(frame*speed/FPS)), 30, 5)
+        pygame.draw.circle(screen, (255, 255, 255), (205-offset, 200+4*math.sin(frame*speed/FPS)), 18)
+        pygame.draw.circle(screen, (255, 255, 255), (205+offset, 210+4*math.sin(frame*speed/FPS)), 30, 5)
+        pygame.draw.circle(screen, (255, 255, 255), (205+offset, 210+4*math.sin(frame*speed/FPS)), 18)
+        return
+    match eyes:
+        case EyesDirection.LEFT_TOP:
+            pygame.draw.circle(screen, (255, 255, 255), (195-offset, 190+4*math.sin(frame*speed/FPS)), 30, 5)
+            pygame.draw.circle(screen, (255, 255, 255), (190-offset, 185+4*math.sin(frame*speed/FPS)), 18)
+            pygame.draw.circle(screen, (255, 255, 255), (195+offset, 200+4*math.sin(frame*speed/FPS)), 30, 5)
+            pygame.draw.circle(screen, (255, 255, 255), (190+offset, 195+4*math.sin(frame*speed/FPS)), 18)
+            return
+        case EyesDirection.FORWARD:
+            pygame.draw.circle(screen, (255, 255, 255), (205-offset, 200-20*math.sin(frame*speed*4/FPS)), 30, 5)
+            pygame.draw.circle(screen, (255, 255, 255), (205-offset, 200-20*math.sin(frame*speed*4/FPS)), 18)
+            pygame.draw.circle(screen, (255, 255, 255), (205+offset, 210+20*math.sin(frame*speed*4/FPS)), 30, 5)
+            pygame.draw.circle(screen, (255, 255, 255), (205+offset, 210+20*math.sin(frame*speed*4/FPS)), 18)
 
 text_surfaces = [NORMAL_FONT.render("C", True, (255, 255, 255)), TALKING_FONT.render("O", True, (255, 255, 255))]
 while running:
@@ -28,6 +53,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Left click
+                eyes = EyesDirection.LEFT_TOP
+            elif event.button == 3:  # Left click
+                eyes = EyesDirection.FORWARD
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
@@ -37,16 +67,7 @@ while running:
     for point in mymath.points:
         pygame_point = mymath.project(point, WIDTH, HEIGHT, angle)
         screen.blit(text_surfaces[1 if mic.talking_frames>0 else 0], pygame_point)
-    if mic.talking_frames>0:
-        pygame.draw.circle(screen, (255, 255, 255), (195-offset, 190+4*math.sin(frame*speed/FPS)), 30, 5)
-        pygame.draw.circle(screen, (255, 255, 255), (190-offset, 185+4*math.sin(frame*speed/FPS)), 18)
-        pygame.draw.circle(screen, (255, 255, 255), (195+offset, 200+4*math.sin(frame*speed/FPS)), 30, 5)
-        pygame.draw.circle(screen, (255, 255, 255), (190+offset, 195+4*math.sin(frame*speed/FPS)), 18)
-    else:
-        pygame.draw.circle(screen, (255, 255, 255), (205-offset, 200+4*math.sin(frame*speed/FPS)), 30, 5)
-        pygame.draw.circle(screen, (255, 255, 255), (205-offset, 200+4*math.sin(frame*speed/FPS)), 18)
-        pygame.draw.circle(screen, (255, 255, 255), (205+offset, 210+4*math.sin(frame*speed/FPS)), 30, 5)
-        pygame.draw.circle(screen, (255, 255, 255), (205+offset, 210+4*math.sin(frame*speed/FPS)), 18)
+    draw_eyes()
     # screen.blit(text_surfaces[2], (180, 180))
     # RENDER YOUR GAME HERE
 
--- mic.py
+++ mic.py
@@ -2,14 +2,14 @@ import numpy as np
 import sounddevice as sd
 
 talking_frames = 0
+TRIGGER_VOLUME = 15
 
 def callback(indata, frames, time, status):
     global talking_frames
     volume_norm = np.linalg.norm(indata) * 10
-    #if SETTINGS["print_loudness"]:
-    #print(f"Loudness: {round(volume_norm*1000)/1000}")
-    if volume_norm > 2:
-        talking_frames = 15
+    # print(f"Loudness: {round(volume_norm*1000)/1000}")
+    if volume_norm > TRIGGER_VOLUME:
+        talking_frames = 12
 
 stream = sd.InputStream(callback=callback)
 stream.start()
